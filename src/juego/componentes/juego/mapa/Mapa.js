2a3dfcc40620f2f8447a7c86c324fc13cafafd23
Luis E Ramos Grados <37456516+LuisERG@users.noreply.github.com>
2019-12-11 23:48:49 -0500

    Update Mapa.js

--- src/juego/componentes/juego/mapa/Mapa.js
+++ src/juego/componentes/juego/mapa/Mapa.js
@@ -3,7 +3,9 @@ import { render } from 'react-dom'
 import L from 'leaflet';
 import { Map, Marker, Popup, TileLayer } from 'react-leaflet'
 import "bootstrap/dist/css/bootstrap.min.css";
+import Axios from 'axios';
 
+const linkForAnswer = "http//as.sc"
 export const pointerIcon = new L.Icon({
     iconUrl: require('../iconos/voulvas.jpg'),
     iconAnchor: [12.5, 25],
@@ -11,20 +13,28 @@ export const pointerIcon = new L.Icon({
     iconSize: [25, 25],
 })
 
+const abc = {
+    respuestas: [
+        "uabsbaskjnasfsa",
+        "basfasafsdsaaa",
+        "casfasaaaa"
+    ],
+}
+
 //pokemonIcon define los iconos que van a ir en los marcadores 
 //en mapa apartir del nombre del pokemon
-function pokemonIcon(name){
+function pokemonIcon(name) {
     return new L.Icon({
-        iconUrl: require('../iconos/'+name+'.jpg'),
+        iconUrl: require('../iconos/' + name + '.jpg'),
         iconAnchor: [12.5, 25],
         popupAnchor: [0, -25],
         iconSize: [25, 25],
     })
 }
 
-function pokeMarkers(){
-    return <Marker/>
-    
+function pokeMarkers() {
+    return <Marker />
+
 }
 
 class Mapa extends Component {
@@ -32,24 +42,65 @@ class Mapa extends Component {
         lat: 51.505,
         lng: -0.09,
         zoom: 18,
+        selectedOption:"default",
+    }
+
+    handleOptionChange = changeEvent => {
+        this.setState({
+          selectedOption: changeEvent.target.value
+        });
+      };
+    
+    handleFormSubmit = formSubmitEvent => {
+        formSubmitEvent.preventDefault();
+        const respuestaReq = {
+            token: "random",
+            id: "ideuser",
+            respuesta: this.state.selectedOption,
+        } 
+        // Axios.post(linkForAnswer, respuestaReq)
+        // .then(res =>{
+        //     console.log(res);
+        //     console.log(res.resultado)
+        //     console.log(res.actualUser)
+        // })
+        console.log("You have submitted:", this.state.selectedOption);
+    };
+
+    showOptions(options, selectedOption) {
+        return options.map((option)=>
+            <div className="custom-control custom-radio">
+                <input type="radio" id={`default-${option}`} name="customRadio" className="custom-control-input"
+                 value={option}
+                 checked={selectedOption === `default-${option}`}
+                 onChange={this.handleOptionChange}/>
+                <label class="custom-control-label" for={`default-${option}`}>
+                    {option}
+                </label>
+            </div>
+        );
     }
 
     render() {
         const usrPosition = [this.props.userLocation.lat, this.props.userLocation.lng]
         const pokeData = this.props.pokeData
-        console.log("usrposu",usrPosition)
+        console.log("usrposu", usrPosition)
         if (pokeData[0]) {
             console.log("pokedata defined")
             pokeMarkers = pokeData.map((pokeInfo) =>
-            <Marker position={[pokeInfo.position.x,pokeInfo.position.y]} icon={pokemonIcon(pokeInfo.name)}>
-                <Popup>
-                    {pokeInfo.question}<br />
-                    <form className="form-inline mt-2 mt-md-0">
-				        <input className="form-control form-control-sm" type="text" placeholder="Respuesta" aria-label="Respuesta"/>
-				    </form>
-                    <button type="submit" class="btn btn-primary btn-sm">Enviar</button>
-                </Popup>
-            </Marker>
+                <Marker position={[pokeInfo.position.x, pokeInfo.position.y]} icon={pokemonIcon(pokeInfo.name)}>
+                    <Popup>
+                        <form onSubmit={this.handleFormSubmit}>
+                        <div className="form-group">
+                            <label>{pokeInfo.question}</label>
+                        </div>
+                        {this.showOptions(abc.respuestas, this.state.selectedOption)}
+                        <div className="form-group">
+                            <button type="submit" class="btn btn-primary btn-sm" >Enviar</button>
+                        </div>
+                        </form>
+                    </Popup>
+                </Marker>
             );
         }
         return (
@@ -69,4 +120,3 @@ class Mapa extends Component {
     }
 }
 export default Mapa;
-
