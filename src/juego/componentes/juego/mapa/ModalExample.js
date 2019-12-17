205b5d4026096b49db6c3173460781aac98b95be
Carlos <espinoza_mansilla@hotmail.com>
2019-12-17 11:36:21 -0500

    Perfil redirection

--- src/juego/componentes/juego/mapa/ModalExample.js
+++ src/juego/componentes/juego/mapa/ModalExample.js
@@ -1,43 +1,44 @@
 /* eslint react/no-multi-comp: 0, react/prop-types: 0 */
 
-import React, { useState } from 'react';
-import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
-import { withRouter } from 'react-router-dom';
+import React, { useState } from "react";
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from "reactstrap";
+import { withRouter } from "react-router-dom";
 
-const ModalExample = (props) => {
-  const {
-    buttonLabel = "Enviar",
-    className = "goooo"
-  } = props;
+const ModalExample = props => {
+  const { buttonLabel = "Enviar", className = "goooo" } = props;
 
   const [modal, setModal] = useState(false);
 
-  function validar(){
-      {toggle()}
+  function validar() {
+    {
+      toggle();
+    }
     //   const { history } = props;
     //   const { from } = { from: { pathname: "/index" } };
     //     history.push(from);
-
   }
   const toggle = () => setModal(!modal);
-  
-  
+
   return (
     <div>
-        {console.log(props.type)}
-      <Button type={props.type} color="primary" onClick={toggle}>{buttonLabel}</Button>
+      {console.log(props.type)}
+      <Button type={props.type} color="primary" onClick={toggle}>
+        {buttonLabel}
+      </Button>
       <Modal isOpen={modal} toggle={toggle} className={className}>
         <ModalHeader toggle={toggle}>Felicitaciones</ModalHeader>
-        <ModalBody>
-          Pokemon capturado, puede revisar su perfil.
-        </ModalBody>
+        <ModalBody>Pokemon capturado, puede revisar su perfil.</ModalBody>
         <ModalFooter>
-          <Button color="primary" onClick={validar}>Ir a perfil</Button>{' '}
-          <Button color="secondary" onClick={toggle}>Seguir</Button>
+          <Button href="./game/perfil" color="primary" onClick={validar}>
+            Ir a perfil
+          </Button>{" "}
+          <Button color="secondary" onClick={toggle}>
+            Seguir
+          </Button>
         </ModalFooter>
       </Modal>
     </div>
   );
-}
+};
 
 export default ModalExample;
